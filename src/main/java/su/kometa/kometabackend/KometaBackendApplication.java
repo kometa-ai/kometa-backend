b829660c11dff6ee8a00ac69d766a7e08c017b2b
nelifs <nelifs@icloud.com>
2025-04-29 21:53:08 +0300

    Initial commit

---
+++ src/main/java/su/kometa/kometabackend/KometaBackendApplication.java
@@ -0,0 +1,13 @@
+package su.kometa.kometabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KometaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KometaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/su/kometa/kometabackend/KometaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package su.kometa.kometabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KometaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
