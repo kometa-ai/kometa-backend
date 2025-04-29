7a5a68f4703920c767ea5eefb6255dfde2c14b92
nelifs <nelifs@icloud.com>
2025-04-30 00:32:06 +0300

    Add base services

---
+++ src/main/java/su/kometa/kometabackend/services/MessageService.java
@@ -0,0 +1,8 @@
+
+package su.kometa.kometabackend.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageService {
+}
---
+++ src/main/java/su/kometa/kometabackend/services/ModelService.java
@@ -0,0 +1,7 @@
+package su.kometa.kometabackend.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ModelService {
+}
