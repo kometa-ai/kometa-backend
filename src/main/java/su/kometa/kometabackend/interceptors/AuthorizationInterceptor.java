8a94ef8ee8258472dfc36b5d86fe47f9be410f8a
notweuz <weuz.main@gmail.com>
2025-05-02 21:49:51 +0400

    Add interceptor

---
+++ src/main/java/su/kometa/kometabackend/interceptors/AuthorizationInterceptor.java
@@ -0,0 +1,40 @@
+package su.kometa.kometabackend.interceptors;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import su.kometa.kometabackend.constants.AttributesConstants;
+import su.kometa.kometabackend.models.User;
+import su.kometa.kometabackend.services.AuthorizationService;
+
+import java.util.Objects;
+
+@Slf4j
+@Component
+public class AuthorizationInterceptor implements HandlerInterceptor {
+
+    private AuthorizationService authorizationService;
+
+    @Autowired
+    public AuthorizationInterceptor(AuthorizationService authorizationService) {
+        this.authorizationService = authorizationService;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler) {
+        if (Objects.equals(request.getMethod(), HttpMethod.OPTIONS.name())) return true;
+
+        String accessToken = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        User user = authorizationService.authUser(accessToken);
+
+        request.setAttribute(AttributesConstants.USER, user);
+        return true;
+    }
+}
