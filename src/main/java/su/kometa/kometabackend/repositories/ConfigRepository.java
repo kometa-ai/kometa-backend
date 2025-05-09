a0e07082a1b20e8d8c08dbb62249f46c8674cc9c
notweuz <weuz.main@gmail.com>
2025-05-10 00:13:27 +0400

    Add config repository

---
+++ src/main/java/su/kometa/kometabackend/repositories/ConfigRepository.java
@@ -0,0 +1,15 @@
+package su.kometa.kometabackend.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import su.kometa.kometabackend.models.Config;
+
+import java.util.Optional;
+
+@Repository
+public interface ConfigRepository extends CrudRepository<Config, Long> {
+
+    Optional<Config> findByName(String name);
+
+    void deleteByName(String name);
+}
