6ace137638c1bed350ac3eb0684e386a333a6ba0
notweuz <weuz.main@gmail.com>
2025-05-10 00:09:39 +0400

    Add config db entity

---
+++ src/main/java/su/kometa/kometabackend/models/Config.java
@@ -0,0 +1,22 @@
+package su.kometa.kometabackend.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "configs")
+public class Config {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String value;
+}
